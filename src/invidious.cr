bc5d844cef5d88ffc00a362c53c211d7603781c7
Chris Harvey <chris@chrisnharvey.com>
2020-05-03 17:57:28 +0100

    Disallow guest access

--- src/invidious.cr
+++ src/invidious.cr
@@ -355,6 +355,12 @@ before_all do |env|
   end
 
   env.set "current_page", URI.encode_www_form(current_page)
+
+  unregistered_path_whitelist = {"/", "/login", "/licenses", "/privacy"}
+  if !env.get?("user") && !unregistered_path_whitelist.includes?(env.request.path)
+    env.response.headers["Location"] = "/login"
+    halt env, status_code: 302
+  end
 end
 
 get "/" do |env|
